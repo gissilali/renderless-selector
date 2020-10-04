5c26eee25703eb39f1767c7ac203ebb83e68ee1b
silali <gibsonsilali@gmail.com>
2020-10-04 22:47:36 +0300

    can handle a delayed data source

--- App.vue
+++ App.vue
@@ -1,88 +1,35 @@
 <template>
   <div>
-    <renderless-selector
-        :search-keys='["name", "email"]'
-        v-model="selectedClient"
-        search-input-id="employee_attendance_user"
-        :options="[
-                            {'name': 'Gibson', 'email': 'gibsonsilali@gmail.com'}
-                        ]"
-        :multiple="false"
-    >
-      <div
-          style="max-width: 300px"
-          class="position-relative"
-          slot-scope="{
-                        selectableItems,
-                        searchResults,
-                        selectedItems,
-                        hasPermission,
-                        searchInputId,
-                        addItemToSelection,
-                        deleteItemFromSelection,
-                        search,
-                        highlightedIndex,
-                        highlightNext,
-                        highlightPrev,
-                        selectItem
-                    }"
-      >
-        <div style="display:flex" class=" align-items-center">
+    <renderless-selector :search-keys='["name"]' v-model="selectedClient" search-input-id="employee_attendance_user"
+      :options="options" :multiple="true">
+      <div class="position-relative"
+        slot-scope="{ selectableItems, searchResults, selectedItems, hasPermission, searchInputId, addItemToSelection, deleteItemFromSelection, search, highlightedIndex, highlightNext, highlightPrev, selectItem }">
+        <div class=" align-items-center">
           <div class="flex-1">
-            <input
-                :id="searchInputId"
-                @keyup.prevent="search"
-                @keydown.down.prevent="highlightNext"
-                @keydown.up.prevent="highlightPrev"
-                @keydown.enter.prevent="selectItem"
-                placeholder="type in employee name and select"
-                autocomplete="npe"
-                type="text"
-                class="form-control form-control-sm"
-            />
-            <div
-                v-show="searchResults.length > 0"
-                class="dropdown-list px-2 py-2  shadow-sm border mt-1 position-absolute bg-white"
-                style="z-index:100; width: 100%"
-            >
-              <div
-
-                  class="dropdown-list-item rounded"
-                  :key="index"
-                  v-for="(result, index) in searchResults"
-              >
-                <a
-                    href="#"
-                    @click.prevent="
-                                            addItemToSelection(result.item)
-                                        "
-                    :class="{'bg-azure-dim': index == highlightedIndex }"
-                    class="user-card mb-2 p-1   rounded hoverable"
-                >
-                  <div
-                      class="user-avatar user-avatar-sm flex-grow-0 bg-primary"
-                  >
-                                            <span>
-                                                {{ result.item.name[0] }}
-                                            </span>
+            <input :id="searchInputId" @keyup.prevent="search" @keydown.down.prevent="highlightNext"
+              @keydown.up.prevent="highlightPrev" @keydown.enter.prevent="selectItem"
+              placeholder="type in employee name and select" autocomplete="npe" type="text" class="form-control" />
+            <div v-show="searchResults.length > 0"
+              class="dropdown-list px-2 py-2  shadow-sm border mt-1 position-absolute bg-white"
+              style="z-index:100; width: 100%">
+              <div class="dropdown-list-item rounded" :key="index" v-for="(result, index) in searchResults">
+                <a href="#" @click.prevent="addItemToSelection(result.item)"
+                  :class="{'bg-azure-dim': index == highlightedIndex }" class="user-card mb-2 p-1   rounded hoverable">
+                  <div class="user-avatar user-avatar-sm flex-grow-0 bg-primary">
+                    <span>
+                      {{ result.item.name[0] }}
+                    </span>
                   </div>
                   <div class="user-info flex-1">
-                                            <span class="tb-lead"
-                                            >{{
-                                                result.item.name
-                                              }}
+                    <span class="tb-lead">{{ result.item.name
+                                                }}
 
-                                                <span
-                                                    class="dot dot-success d-md-none ml-1"
-                                                ></span>
-                                            </span>
+                      <span class="dot dot-success d-md-none ml-1"></span>
+                    </span>
                   </div>
                 </a>
               </div>
             </div>
-            <ul>
-              <li :key="index"  v-for="(selectedItem, index) in selectedItems">{{ selectedItem.name }}</li>
-            </ul>
           </div>
         </div>
       </div>
@@ -91,16 +38,19 @@
 </template>
 
 <script>
-import RenderlessSelector from "./src/entry";
-export default {
-  name: "App",
-  components: {RenderlessSelector},
-  data() {
-    return {
-      selectedClient: null
+  import RenderlessSelector from "./src/entry";
+  export default {
+    name: "App",
+    components: {
+      RenderlessSelector
+    },
+    data() {
+      return {
+        selectedClient: null,
+        options: [{"id":1,"specialty_id":null,"name":"Allergy & Immunology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":2,"specialty_id":null,"name":"Anesthesiology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":3,"specialty_id":null,"name":"Dermatology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":4,"specialty_id":null,"name":"Diagnostic Radiology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":5,"specialty_id":null,"name":"Emergency Medicine","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":6,"specialty_id":null,"name":"Family Medicine","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":7,"specialty_id":null,"name":"Internal Medicine","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":8,"specialty_id":null,"name":"Medical Genetics","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":9,"specialty_id":null,"name":"Neurology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":10,"specialty_id":null,"name":"Nuclear Medicine","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":11,"specialty_id":null,"name":"Obstetrics And Gynecology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":12,"specialty_id":null,"name":"Ophthalmology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":13,"specialty_id":null,"name":"Pathology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":14,"specialty_id":null,"name":"Pediatrics","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":15,"specialty_id":null,"name":"Physical Medicine & Rehabilitation","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":16,"specialty_id":null,"name":"Preventive Medicine","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":17,"specialty_id":null,"name":"Psychiatry","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":18,"specialty_id":null,"name":"Radiation Oncology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":19,"specialty_id":null,"name":"Surgery","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"},{"id":20,"specialty_id":null,"name":"Urology","is_active":true,"created_at":"2020-10-04T18:35:37.000000Z","updated_at":"2020-10-04T18:35:37.000000Z"}]
+      }
     }
   }
-}
 </script>
 
 <style scoped>
--- src/RenderlessSelector.vue
+++ src/RenderlessSelector.vue
@@ -1,194 +1,210 @@
 <script>
-import Fuse from "fuse.js";
+  import Fuse from "fuse.js";
 
-export default {
-  name: "RenderlessSelector",
-  props: {
-    resultLimit: {
-      default: 5
-    },
-    searchKeys: {
-      required: true,
-      type: Array
-    },
-    options: {
-      required: true,
-      type: Array
-    },
-    multiple: {
-      default: true
-    },
-    searchInputId: {
-      required: true
-    },
-    allowDuplicates: {
-      default: false
-    }
-  },
-  created() {
-    this.getSelectableItems();
-    if (this.multiple) {
-      this.selectedItems = this.$attrs.value;
-    } else {
-      if (this.$attrs.value) {
-        this.selectedItems = [this.$attrs.value];
-      } else {
-        this.selectedItems = [];
+  export default {
+    name: "RenderlessSelector",
+    props: {
+      resultLimit: {
+        default: 5
+      },
+      searchKeys: {
+        required: true,
+        type: Array
+      },
+      options: {
+        required: true,
+        type: Array
+      },
+      multiple: {
+        default: true
+      },
+      searchInputId: {
+        required: true
+      },
+      allowDuplicates: {
+        default: false
       }
-    }
-
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.searchInput = this.$el.querySelector(`#${this.searchInputId}`);
-    });
-  },
-  data() {
-    return {
-      selectableItems: [],
-      fuseSearch: null,
-      searchResults: [],
-      selectedItems: [],
-      hasPermission: true,
-      searchInput: null,
-      highlightedIndex: 0
-    };
-  },
-  methods: {
-    getSelectableItems() {
-      this.selectableItems = this.options;
-      let options = {
-        shouldSort: true,
-        minMatchCharLength: 3,
-        keys: [...this.searchKeys]
-      };
-
-      this.fuseSearch = new Fuse(this.selectableItems, options);
     },
-    search(event) {
-      if (this.fuseSearch) {
-        this.searchResults = this.fuseSearch.search(event.target.value).slice(0, this.resultLimit);
-      }
-
-      return [];
-    },
-    addItemToSelection(item) {
+    created() {
       if (this.multiple) {
-        if (this.allowDuplicates) {
-          this.selectedItems.push(item);
-        } else {
-          let hasSelectedItem = false;
-          for (let i = 0; i < this.selectedItems.length; i++) {
-            if (this.selectedItems[i].id == item.id) {
-              hasSelectedItem = true;
-              break;
-            }
-          }
-
-          if (!hasSelectedItem) {
-            this.selectedItems.push(item);
-          }
-        }
+        this.selectedItems = this.$attrs.value;
       } else {
-        this.selectedItems = [];
-        this.selectedItems.push(item);
-      }
-      this.searchResults = [];
-      this.searchInput.value = "";
-      this.$emit('item-added', item)
-    }, selectItem() {
-      this.addItemToSelection(this.searchResults[this.highlightedIndex].item)
-    }, deleteItemFromSelection(item) {
-      this.selectedItems.filter(selectedItem => {
-        return selectedItem.id !== item.id;
-      })
-      this.$emit('item-removed', item)
-    },
-    highlightNext() {
-      this.highlight(this.highlightedIndex + 1);
-    },
-    highlightPrev() {
-      this.highlight(this.highlightedIndex - 1);
-    },
-    highlight(index) {
-      this.highlightedIndex = index;
-
-      if (this.highlightedIndex < 0) {
-        this.highlightedIndex = this.searchResults.length - 1
-      }
-      if
-      (this.highlightedIndex > this.searchResults.length - 1) {
-        this.highlightedIndex = 0
-      }
-
-      this.input = this.searchResults[this.highlightedIndex]
-    },
-    handleEvent() {
-      this.$emit('items-modified',)
-    },
-    containsObject(obj, list) {
-      let i;
-      for (i = 0; i < list.length; i++) {
-        if (list[i] === obj) {
-          return true;
-        }
-      }
-      return false;
-    }
-  }, watch: {
-    selectedItems: {
-      handler: function (value) {
-        if (this.multiple) {
-          this.$emit("input", value);
+        if (this.$attrs.value) {
+          this.selectedItems = [this.$attrs.value];
         } else {
-          if (value.length > 0) {
-            this.$emit("input", value[0])
-          }
+          this.selectedItems = [];
         }
       }
+
     },
-    selectableItems: {
-      handler: function (items) {
+    mounted() {
+      this.$nextTick(() => {
+        this.searchInput = this.$el.querySelector(`#${this.searchInputId}`);
+      });
+    },
+    data() {
+      return {
+        selectableItems: [],
+        fuseSearch: null,
+        searchResults: [],
+        selectedItems: [],
+        hasPermission: true,
+        searchInput: null,
+        highlightedIndex: 0
+      };
+    },
+    methods: {
+      getSelectableItems() {
+        this.selectableItems = this.options;
+        console.log(this.selectableItems);
         let options = {
           shouldSort: true,
           minMatchCharLength: 3,
+          threshold: 1.0,
           keys: [...this.searchKeys]
         };
-        this.fuseSearch = new Fuse(items, options);
+
+        this.fuseSearch = new Fuse(this.selectableItems, options);
+      },
+      search(event) {
+        if (this.fuseSearch) {
+          console.log(this.fuseSearch.search(event.target.value));
+          this.searchResults = this.fuseSearch.search(event.target.value);
+          return;
+        }
+
+        return [];
+      },
+      addItemToSelection(item) {
+        if (this.multiple) {
+          if (this.allowDuplicates) {
+            this.selectedItems.push(item);
+          } else {
+            let hasSelectedItem = false;
+            if (Array.isArray(this.selectedItems) === false && this.selectedItems == null) {
+              this.selectedItems = [];
+            }
+
+            for (let i = 0; i < this.selectedItems.length; i++) {
+              if (this.selectedItems[i].id == item.id) {
+                hasSelectedItem = true;
+                break;
+              }
+            }
+
+
+            if (!hasSelectedItem) {
+              this.selectedItems.push(item);
+            }
+          }
+        } else {
+          this.selectedItems = [];
+          this.selectedItems.push(item);
+        }
+        this.searchResults = [];
+        this.searchInput.value = "";
+        this.$emit('item-added', item)
+      },
+      selectItem() {
+        this.addItemToSelection(this.searchResults[this.highlightedIndex].item)
+      },
+      deleteItemFromSelection(item) {
+        this.selectedItems.filter(selectedItem => {
+          return selectedItem.id !== item.id;
+        })
+        this.$emit('item-removed', item)
+      },
+      highlightNext() {
+        this.highlight(this.highlightedIndex + 1);
+      },
+      highlightPrev() {
+        this.highlight(this.highlightedIndex - 1);
+      },
+      highlight(index) {
+        this.highlightedIndex = index;
+
+        if (this.highlightedIndex < 0) {
+          this.highlightedIndex = this.searchResults.length - 1
+        }
+        if (this.highlightedIndex > this.searchResults.length - 1) {
+          this.highlightedIndex = 0
+        }
+
+        this.input = this.searchResults[this.highlightedIndex]
+      },
+      handleEvent() {
+        this.$emit('items-modified', )
+      },
+      containsObject(obj, list) {
+        let i;
+        for (i = 0; i < list.length; i++) {
+          if (list[i] === obj) {
+            return true;
+          }
+        }
+        return false;
       }
     },
-    '$attrs.value': {
-      handler: function (selectedItems) {
-        if (selectedItems) {
-          if (Array.isArray(selectedItems)) {
-            this.selectedItems = selectedItems;
+    watch: {
+      selectedItems: {
+        handler: function (value) {
+          if (this.multiple) {
+            this.$emit("input", value);
           } else {
-            if (!this.containsObject(selectedItems, this.selectableItems)) {
-              this.selectableItems.push(selectedItems);
-
+            if (value.length > 0) {
+              this.$emit("input", value[0])
             }
-            this.selectedItems = [selectedItems];
           }
         }
       },
-      immediate: true
+      selectableItems: {
+        handler: function (items) {
+          let options = {
+            shouldSort: true,
+            minMatchCharLength: 3,
+            keys: [...this.searchKeys]
+          };
+          this.fuseSearch = new Fuse(items, options);
+        }
+      },
+      '$attrs.value': {
+        handler: function (selectedItems) {
+          if (selectedItems) {
+            if (Array.isArray(selectedItems)) {
+              this.selectedItems = selectedItems;
+            } else {
+              if (!this.containsObject(selectedItems, this.selectableItems)) {
+                this.selectableItems.push(selectedItems);
+
+              }
+              this.selectedItems = [selectedItems];
+            }
+          }
+        },
+        immediate: true
+      },
+      'options': {
+        handler: function () {
+          this.getSelectableItems();
+        },
+        immediate: true 
+      }
+    },
+    render() {
+      return this.$scopedSlots.default({
+        selectableItems: this.selectableItems,
+        searchResults: this.searchResults,
+        selectedItems: this.selectedItems,
+        hasPermission: this.hasPermission,
+        searchInputId: this.searchInputId,
+        addItemToSelection: this.addItemToSelection,
+        deleteItemFromSelection: this.deleteItemFromSelection,
+        search: this.search,
+        highlightNext: this.highlightNext,
+        highlightPrev: this.highlightPrev,
+        selectItem: this.selectItem,
+        highlightedIndex: this.highlightedIndex,
+      });
     }
-  },
-  render() {
-    return this.$scopedSlots.default({
-      selectableItems: this.selectableItems,
-      searchResults: this.searchResults,
-      selectedItems: this.selectedItems,
-      hasPermission: this.hasPermission,
-      searchInputId: this.searchInputId,
-      addItemToSelection: this.addItemToSelection,
-      deleteItemFromSelection: this.deleteItemFromSelection,
-      search: this.search,
-      highlightNext: this.highlightNext,
-      highlightPrev: this.highlightPrev,
-      selectItem: this.selectItem,
-      highlightedIndex: this.highlightedIndex,
-    });
-  }
-};
+  };
 </script>
