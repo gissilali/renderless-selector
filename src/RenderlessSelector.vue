7916641145862fee8340a857fa464fe804e8eed9
Silali <gibson@boya.co>
2023-03-27 14:44:24 +0300

    added duplicateKey prop to allow specification of a key that will be used to check for duplicate objects in the selectedItems Array

--- src/RenderlessSelector.vue
+++ src/RenderlessSelector.vue
@@ -1,63 +1,66 @@
 <script>
-import Fuse from "fuse.js";
-import {handleClickOutside} from "./utils/clickOutsideHandler";
+import Fuse from 'fuse.js'
+import { handleClickOutside } from './utils/clickOutsideHandler'
 
 export default {
-  name: "RenderlessSelector",
+  name: 'RenderlessSelector',
   props: {
     resultLimit: {
-      default: 5
+      default: 5,
     },
     searchKeys: {
       required: true,
-      type: Array
+      type: Array,
     },
     options: {
       required: true,
-      type: Array
+      type: Array,
     },
     multiple: {
-      default: true
+      default: true,
     },
     searchInputId: {
-      required: true
+      required: true,
     },
     allowDuplicates: {
-      default: false
+      default: false,
     },
     isSearchable: {
       type: Boolean,
-      default: true
-    }
+      default: true,
+    },
+    duplicateKey: {
+      type: String,
+      default: 'id',
+    },
   },
   created() {
     if (this.multiple) {
-      this.selectedItems = this.$attrs.value;
+      this.selectedItems = this.$attrs.value
     } else {
       if (this.$attrs.value) {
-        this.selectedItems = [this.$attrs.value];
+        this.selectedItems = [this.$attrs.value]
       } else {
-        this.selectedItems = [];
+        this.selectedItems = []
       }
     }
-
   },
   mounted() {
     this.$nextTick(() => {
       if (this.isSearchable) {
-        this.searchInput = this.$el.querySelector(`#${this.searchInputId}`);
+        this.searchInput = this.$el.querySelector(`#${this.searchInputId}`)
         this.searchInput.addEventListener('focus', () => {
           this.searchResults = this.options.map((item, index) => {
-            return {item, refIndex: index}
+            return { item, refIndex: index }
           })
         })
       } else {
-        const searchInput = this.$el.querySelector(`#${this.searchInputId}`);
+        const searchInput = this.$el.querySelector(`#${this.searchInputId}`)
         searchInput.setAttribute('readonly', 'true')
         this.searchInput = searchInput
         this.searchInput.addEventListener('click', () => {
           const searchResults = this.options.map((item, index) => {
-            return {item, refIndex: index}
+            return { item, refIndex: index }
           })
           if (this.searchResults.length === 0) {
             this.searchResults = searchResults
@@ -69,10 +72,8 @@ export default {
       handleClickOutside(this.searchInput, () => {
         this.searchResults = []
         this.searchInput.value = ''
-      });
-
-
-    });
+      })
+    })
   },
   data() {
     return {
@@ -83,20 +84,20 @@ export default {
       hasPermission: true,
       searchInput: null,
       highlightedIndex: 0,
-      searchQuery: ''
-    };
+      searchQuery: '',
+    }
   },
   methods: {
     getSelectableItems() {
-      this.selectableItems = this.options;
-      console.log(this.selectableItems);
+      this.selectableItems = this.options
+      console.log(this.selectableItems)
 
       this.fuseSearch = new Fuse(this.selectableItems, {
         shouldSort: true,
         minMatchCharLength: 3,
         threshold: 1.0,
-        keys: [...this.searchKeys]
-      });
+        keys: [...this.searchKeys],
+      })
     },
     search(event) {
       this.searchQuery = event.target.value
@@ -104,69 +105,75 @@ export default {
         if (event.target.value.length > 0) {
           const results = this.fuseSearch.search(event.target.value)
           if (results.length > 0) {
-            this.searchResults = this.fuseSearch.search(event.target.value);
+            this.searchResults = this.fuseSearch.search(event.target.value)
           }
 
-          return;
+          return
         }
 
         this.searchResults = this.options.map((item, index) => {
-          return {item, refIndex: index}
+          return { item, refIndex: index }
         })
       }
 
-      return [];
+      return []
     },
     addItemToSelection(item, clearSearch = true) {
       if (clearSearch) {
-        this.searchQuery = ""
+        this.searchQuery = ''
       }
       if (this.multiple) {
         if (this.allowDuplicates) {
-          this.selectedItems.push(item);
+          this.selectedItems.push(item)
         } else {
-          let hasSelectedItem = false;
-          if (Array.isArray(this.selectedItems) === false && this.selectedItems == null) {
-            this.selectedItems = [];
+          let hasSelectedItem = false
+          if (
+              Array.isArray(this.selectedItems) === false &&
+              this.selectedItems == null
+          ) {
+            this.selectedItems = []
           }
 
           for (let i = 0; i < this.selectedItems.length; i++) {
-            if (this.selectedItems[i].id == item.id) {
-              hasSelectedItem = true;
-              break;
+            if (
+                this.selectedItems[i][this.duplicateKey] ==
+                item[this.duplicateKey]
+            ) {
+              hasSelectedItem = true
+              break
             }
           }
 
-
           if (!hasSelectedItem) {
-            this.selectedItems.push(item);
+            this.selectedItems.push(item)
           }
         }
       } else {
-        this.selectedItems = [];
-        this.selectedItems.push(item);
+        this.selectedItems = []
+        this.selectedItems.push(item)
+        this.$emit('item-added', item)
       }
-      this.searchResults = [];
-      this.searchInput.value = "";
+      this.searchResults = []
+      this.searchInput.value = ''
       this.$emit('item-added', item)
     },
     selectItem() {
       this.addItemToSelection(this.searchResults[this.highlightedIndex].item)
     },
     deleteItemFromSelection(item) {
-      this.selectedItems.filter(selectedItem => {
-        return selectedItem.id !== item.id;
+      this.selectedItems.filter((selectedItem) => {
+        return selectedItem.id !== item.id
       })
       this.$emit('item-removed', item)
     },
     highlightNext() {
-      this.highlight(this.highlightedIndex + 1);
+      this.highlight(this.highlightedIndex + 1)
     },
     highlightPrev() {
-      this.highlight(this.highlightedIndex - 1);
+      this.highlight(this.highlightedIndex - 1)
     },
     highlight(index) {
-      this.highlightedIndex = index;
+      this.highlightedIndex = index
 
       if (this.highlightedIndex < 0) {
         this.highlightedIndex = this.searchResults.length - 1
@@ -178,64 +185,64 @@ export default {
       this.input = this.searchResults[this.highlightedIndex]
     },
     handleEvent() {
-      this.$emit('items-modified',)
+      this.$emit('items-modified')
     },
     containsObject(obj, list) {
-      let i;
+      let i
       for (i = 0; i < list.length; i++) {
         if (list[i] === obj) {
-          return true;
+          return true
         }
       }
-      return false;
-    }
+      return false
+    },
   },
   watch: {
     selectedItems: {
       handler: function (value) {
         if (this.multiple) {
-          this.$emit("input", value);
+          console.log('multiple selection....')
+          this.$emit('input', value)
         } else {
           if (value.length > 0) {
-            this.$emit("input", value[0])
+            this.$emit('input', value[0])
           }
         }
-      }
+      },
     },
     selectableItems: {
       handler: function (items) {
         let options = {
           shouldSort: true,
           minMatchCharLength: 1,
-          keys: [...this.searchKeys]
-        };
-        this.fuseSearch = new Fuse(items, options);
-      }
+          keys: [...this.searchKeys],
+        }
+        this.fuseSearch = new Fuse(items, options)
+      },
     },
     '$attrs.value': {
       handler: function (selectedItems) {
         if (selectedItems) {
           if (Array.isArray(selectedItems)) {
-            this.selectedItems = selectedItems;
+            this.selectedItems = selectedItems
           } else {
             if (!this.containsObject(selectedItems, this.selectableItems)) {
-              this.selectableItems.push(selectedItems);
-
+              this.selectableItems.push(selectedItems)
             }
-            this.selectedItems = [selectedItems];
+            this.selectedItems = [selectedItems]
           }
         } else {
-          this.selectedItems = [];
+          this.selectedItems = []
         }
       },
-      immediate: true
+      immediate: true,
     },
-    'options': {
+    options: {
       handler: function () {
-        this.getSelectableItems();
+        this.getSelectableItems()
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   render() {
     return this.$scopedSlots.default({
@@ -251,8 +258,8 @@ export default {
       highlightPrev: this.highlightPrev,
       selectItem: this.selectItem,
       highlightedIndex: this.highlightedIndex,
-      searchQuery: this.searchQuery
-    });
-  }
-};
+      searchQuery: this.searchQuery,
+    })
+  },
+}
 </script>
